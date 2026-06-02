ce61d1992f895c1502f6d00b4ac6c60406397fca
Ali Newaj <91418999+AL1N3WAJ@users.noreply.github.com>
2026-06-02 11:49:56 +0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://AL1N3WAJ.github.io',
-  base: '/alinewaj',
+  site: 'https://al1n3waj.github.io',
+  base: '/alinewaj/',         
   integrations: [tailwind()],
   output: 'static',
   prefetch: true
